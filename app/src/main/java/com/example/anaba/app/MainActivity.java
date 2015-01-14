4e4db0427d511381322c5c97ed264fccb4208fcd
gucci-megane <gucci.magane.tks@gmail.com>
2015-01-14 14:04:12 +0900

    gps機能の追加

--- app/src/main/java/com/example/anaba/app/MainActivity.java
+++ app/src/main/java/com/example/anaba/app/MainActivity.java
@@ -1,23 +1,31 @@
 package com.example.anaba.app;
 
 import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.Window;
 import android.widget.Toast;
 
 import com.astuetz.PagerSlidingTabStrip;
 import com.squareup.otto.Subscribe;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends FragmentActivity implements LocationListener{
+
+    private LocationManager mLocationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.main_layout);
+        mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
 
         FragmentManager manager = getSupportFragmentManager();
 
@@ -35,6 +43,17 @@ public class MainActivity extends FragmentActivity {
         super.onResume();
         // Subscriberとして登録する
         BusHolder.get().register(this);
+
+        if (mLocationManager != null) {
+            Log.v("Check", "check");
+            mLocationManager.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER,
+//                LocationManager.NETWORK_PROVIDER,
+                    0,
+                    0,
+                    this);
+        }
+        super.onResume();
     }
 
     @Override
@@ -42,6 +61,11 @@ public class MainActivity extends FragmentActivity {
         super.onPause();
         // Subscriberの登録を解除する
         BusHolder.get().unregister(this);
+
+        if (mLocationManager != null) {
+            mLocationManager.removeUpdates(this);
+        }
+        super.onPause();
     }
 
     @Subscribe
@@ -49,6 +73,42 @@ public class MainActivity extends FragmentActivity {
         Toast.makeText(this, event.message, Toast.LENGTH_LONG).show();
     }
 
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.v("----------", "----------");
+        Log.v("Latitude", String.valueOf(location.getLatitude()));
+        Log.v("Longitude", String.valueOf(location.getLongitude()));
+        Log.v("Accuracy", String.valueOf(location.getAccuracy()));
+        Log.v("Altitude", String.valueOf(location.getAltitude()));
+        Log.v("Time", String.valueOf(location.getTime()));
+        Log.v("Speed", String.valueOf(location.getSpeed()));
+        Log.v("Bearing", String.valueOf(location.getBearing()));
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        switch (status) {
+            case LocationProvider.AVAILABLE:
+                Log.v("Status", "AVAILABLE");
+                break;
+            case LocationProvider.OUT_OF_SERVICE:
+                Log.v("Status", "OUT_OF_SERVICE");
+                break;
+            case LocationProvider.TEMPORARILY_UNAVAILABLE:
+                Log.v("Status", "TEMPORARILY_UNAVAILABLE");
+                break;
+        }
+    }
 }
 
 
